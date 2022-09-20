96b2872837d1ed12c75665f73d51e67df02eebc6
Tomachi <8929706+book000@users.noreply.github.com>
2022-09-20 19:42:25 +0900

    chore: 依存パッケージ更新

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import config from 'config'
-import { Client, DiscordAPIError, Intents, Message } from 'discord.js'
+import { Client, DiscordAPIError, Message } from 'discord.js'
 
 const client = new Client({
   intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    'Guilds',
+    'GuildMessages',
+    'GuildMessageReactions',
+    'MessageContent',
   ],
 })
 
